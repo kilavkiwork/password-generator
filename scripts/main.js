b5bbb9ea06c50849ee15fb014a834678503b3693
kilavkiwork <kilavki@outlook.com>
2024-12-18 23:19:49 +0200

    wip: code for inputs

--- scripts/main.js
+++ scripts/main.js
@@ -19,3 +19,52 @@ updateRange();
 
 range.addEventListener('input', updateRange)
 // end slider range code
+
+// check inputs
+const inputs = document.querySelector('.inputs');
+const arrInputs = inputs.querySelectorAll('input');
+console.log(arrInputs);
+
+const uppercase = {
+  start: 65,
+  length: 26,
+}
+const lowercase = {
+  start: 97,
+  length: 26,
+}
+
+const objs = [uppercase, lowercase]
+
+
+function checkInput() {
+  let checkInputs = []
+  arrInputs.forEach((input) => {
+    if(input.checked) {
+      checkInputs.push(input.name) 
+    }
+  })
+  return checkInputs
+  console.log(checkInputs);
+}
+// checkInput()
+// inputs.addEventListener('input', checkInput)
+
+function createMainArr() {
+  let a = checkInput()
+  console.log(a);
+  
+}
+
+function createSymbols(length, start) {
+  return Array.from({ length: length }, (_, i) => {
+    return String.fromCharCode(start + i);
+  });
+}
+
+const button = document.querySelector('button.btn');
+console.log(button);
+button.addEventListener('click', () => {
+  // checkInput()
+  createMainArr()
+})
