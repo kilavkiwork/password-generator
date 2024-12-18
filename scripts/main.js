3ba38e87681494a257abfdc5a811896a99433bd5
kilavkiwork <kilavki@outlook.com>
2024-12-18 21:26:09 +0200

    feat: range slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
-console.log('hello console this is script from "./scripts/main.js"');
+// slider range code
+const range = document.querySelector('#range');
+const rangeValue = document.querySelector('#range-value');
+
+function updateRange() {
+  let value = range.value;
+  // розраховуємо відсотки для градієнту у слайдері
+  let percent = ((value - range.min) / (range.max - range.min)) * 100;
+  
+  range.style.setProperty(
+    'background',
+    `linear-gradient(to right, var(--neon_green) ${percent}%, var(--black) ${percent}%`
+  );
+  
+  rangeValue.textContent = value
+}
+
+updateRange();
+
+range.addEventListener('input', updateRange)
+// end slider range code
