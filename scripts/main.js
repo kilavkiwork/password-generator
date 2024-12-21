aaad5a0764a87f74e407a49d7dfde42f12d943b2
kilavkiwork <kilavki@outlook.com>
2024-12-21 21:08:33 +0200

    feat: strength message

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,80 @@
-// slider range code
 const range = document.querySelector('#range');
 const rangeValue = document.querySelector('#range-value');
-
-function updateRange() {
-  let value = range.value;
-  // розраховуємо відсотки для градієнту у слайдері
-  let percent = ((value - range.min) / (range.max - range.min)) * 100;
-  
-  range.style.setProperty(
-    'background',
-    `linear-gradient(to right, var(--neon_green) ${percent}%, var(--black) ${percent}%`
-  );
-  
-  rangeValue.textContent = value;
-}
-
-updateRange();
-
-range.addEventListener('input', updateRange);
-// end slider range code
-// 
-// -----------
-// 
+const output = document.querySelector('#strength');
+const levels = document.querySelectorAll('.rate > span.percentage');
 const inputs = document.querySelector('.inputs');
 const arrInputs = inputs.querySelectorAll('input');
 const button = document.querySelector('button.btn');
-const password = document.querySelector('#pass')
+const password = document.querySelector('#pass');
 
+// Об'єкт для зберігання повідомлень відповідно до сили
+const strengthMessages = {
+  1: 'too weak!',
+  2: 'weak!',
+  3: 'medium',
+  4: 'strong!',
+};
+
+// Об'єкт для зберігання символів
 const chars = {
   lowercase: 'abcdefghijklmnopqrstuvwxyz',
   uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
   numbers: '0123456789',
   symbols: '!@#$%^&*()-_=+[]{};:,<.>/?',
 };
+
+// slider range code
+function updateRange() {
+  let value = range.value;
+  // розраховуємо відсотки для градієнту у слайдері
+  let percent = ((value - range.min) / (range.max - range.min)) * 100;
+
+  range.style.setProperty(
+    'background',
+    `linear-gradient(to right, var(--neon_green) ${percent}%, var(--black) ${percent}%`
+  );
+
+  rangeValue.textContent = value;
+  updateStrength(value);
+}
+
+function updateStrength(value) {
+  value = parseInt(value);
+
+  // Скидаємо стиль для всіх рівнів
+  levels.forEach((item) => {
+    item.style.setProperty('background-color', '');
+    item.style.setProperty('border-color', '');
+  });
+
+  let color, count;
+
+  if (value <= 8) {
+    (color = 'var(--red)'), (count = 1);
+    output.textContent = strengthMessages[count];
+  } else if (value > 8 && value <= 10) {
+    (color = 'var(--orange)'), (count = 2);
+    output.textContent = strengthMessages[count];
+  } else if (value > 10 && value <= 16) {
+    (color = 'var(--yellow)'), (count = 3);
+    output.textContent = strengthMessages[count];
+  } else {
+    (color = 'var(--neon_green)'), (count = 4);
+    output.textContent = strengthMessages[count];
+  }
+
+  Array.from(levels)
+    .slice(0, count)
+    .forEach((item) => {
+      item.style.setProperty('background-color', color);
+      item.style.setProperty('border-color', color);
+    });
+}
+
+updateRange();
+
+range.addEventListener('input', updateRange);
+// end slider range code
 
 // Збираємо вибрані інпути
 function checkInput() {
@@ -72,8 +114,7 @@ button.addEventListener('click', () => {
     return;
   }
   let data = createData(selectedInputs);
-  let passLength = range.value
+  let passLength = range.value;
   let pass = generatePassword(data, passLength);
-  password.textContent = pass
+  password.textContent = pass;
 });
-
